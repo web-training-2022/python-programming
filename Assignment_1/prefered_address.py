b94ffa21b3b43244c016407c0a748e9a4cd73df4
Subhajit Pal <heresubhajit@outlook.com>
2022-07-31 22:09:17 +0530

    ver3

--- Assignment_1/prefered_address.py
+++ Assignment_1/prefered_address.py
@@ -7,9 +7,8 @@ def prefred_address(user):
     users_list = [ key for key in cust_accounts.keys() ]
     if user in users_list:
         pref_add_type = cust_accounts.get(user).get('Address',{}).get('Preferred')
-        pref_add = cust_accounts.get(user).get('Address').get(pref_add_type)
         if not pref_add_type:
-            pass
+            data = "No Address is Mentioned"
         else:
             for values in cust_accounts.get(user).get('Address',{}).get(pref_add_type,{}).values():
                 address.append(values)
@@ -31,6 +30,15 @@ def prefred_address(user):
         
 
 if __name__ == "__main__":
-    name = input('Enter the customer Name or all to get data for everyone\n')
-    pref_address = prefred_address(name)
-    print(f"Prefred address {pref_address}") 
+    name = input('Enter all to see everyone address or specific to see perticular\n')
+    if name == 'all':
+        pref_address = prefred_address(name)
+        print(f"Prefred address: {pref_address}") 
+    elif name == 'specific':
+        name = input('Enter the name of the person\n')
+        pref_address = prefred_address(name)
+        print(f"Prefred address: {pref_address}") 
+    else:
+        print("Something Wrong")
+    
+    
